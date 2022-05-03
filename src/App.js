23148e43e6696b65c1d47d58789d52080bccdf5e
turfguy <ksm0811@ajou.ac.kr>
2022-05-03 20:05:50 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  let post= '블로그 글 제목임';
+  let [글제목,b] = useState('남자 코트 추천');
+  let logo = 'ReactBlog';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="black-nav">
+          <h4>{logo}</h4>
+        </div>
+        <div className= "list">
+            <h4>{글제목}</h4>
+            <p>5월 3일 글임</p>
+        </div>
+      
+      </div>
   );
 }
 

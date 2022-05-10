de18826cc6a4ebcbe07caab8b429fc15478f5d83
turfguy <ksm0811@ajou.ac.kr>
2022-05-10 17:58:26 +0900

    컴포넌트,동적UI,..

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   let post= '블로그 글 제목임';
   let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
   let [따봉,따봉변경] = useState(0);
+  let [modal,setModal] = useState(false);
   let logo = 'ReactBlog';
   return (
     <div className="App">
@@ -16,22 +17,29 @@ function App() {
         </div>
         <button onClick={()=>{
           let change = [...글제목];
-           글제목.sort();
-           글제목변경(글제목);
+           change.sort();
+           글제목변경(change);
         }}>글정렬</button>
 
         <button onClick={()=>{
           let copy = [...글제목];
           copy[0]= '여자 코트 추천'
-          글제목변경(글제목);
+          글제목변경(copy);
           }}>♻️</button>
         <div className= "list">
-            <h4>
+            <h4 onClick={()=>{
+              setModal(!modal)
+            }}>
               {글제목[0]}
             </h4>
               <span onClick={()=>{따봉변경(따봉++)}}>❤️</span> {따봉}
             <p>5월 3일 글임</p>
         </div>
+        <div>
+              {
+                modal == true ? <Modal/> : null
+              }
+            </div>
         <div className= "list">
             <h4>{글제목[1]}</h4>
             <p>5월 3일 글임</p>
@@ -40,9 +48,19 @@ function App() {
             <h4>{글제목[2 ]}</h4>
             <p>5월 3일 글임</p>
         </div>
-      
+        {
+          modal == true ? <Modal/> : null
+        }
       </div>
   );
 }
-
+function Modal(){
+  return(
+    <div className='modal'>
+        <h4>상세페이지</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
+}
 export default App;
